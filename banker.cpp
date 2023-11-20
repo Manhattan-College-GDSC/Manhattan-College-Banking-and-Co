717a50143c5bdd9542643ed3fba6443ff5b6ee0d
Stephen <75145009+kingsteph23@users.noreply.github.com>
2023-11-19 21:05:28 -0500

    Create banker.cpp

---
+++ banker.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+#include <iomanip>
+#include <cstdlib> 
+#include <time.h>
+
+double initbalance; //inital balance for savings/checking
+double balance; //checking balance
+std::string firstname; //firstname of user
+std::string lastname; //lastname of userdouble depo()
+
+
+
+double savingsbalance; //savings balance
+
+//savings
+void savings()
+{
+  std::string transfer;
+	double transferamount;
+
+}
+
+//checking account information
+void checking()
+{
+std::string transfer;
+	double transferamount;
+
+
+  
+}
+//deposit
+double depo()
+{
+
+double depositchoice;
+	std::string input;
+	std::string accountchoice;
+
+  
+  return balance;
+}
+
+//withdrawals
+double withdraw()
+{
+double withdrawalchoice;
+	std::string withdrawchoice;
+	std::string input;
+
+  
+
+return balance;
+}
+
+
+//balance inquiry
+void display()
+{
+int input;
+
+  
+}
+
+
+//transferring money between accounts
+void transfer()
+{
+
+	double transferamount;
+
+	int input;
+	double checkingproblem;
+	double savingsproblem;
+
+  
+}
+
+
+
+// open reg checking/savings account
+int i = 0;
+void openaccount() {
+
+
+}
+
+
+
+
+
+int main(){
+std::string moreaccounts;
+	std::string creation;
+
+  std::string input;
+	double depositchoice;
+
+  
+
+
+return 0;
+}
+
+
+
+
