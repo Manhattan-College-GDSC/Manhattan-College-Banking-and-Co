b3b17abf5c20711478b5d156632cb6e4521099d6
Stephen <75145009+kingsteph23@users.noreply.github.com>
2024-03-30 12:01:19 -0400

    Added some minor changes and bugs: Mainly linebreaks and some minor bugs

--- banker.cpp
+++ banker.cpp
@@ -15,16 +15,16 @@ double savingsbalance; //savings balance
 //savings
 void savings(){ 
   //ask users name
-  cout << "Enter your first name: ";
+  cout << "Enter your first name: " << endl;
   cin >> firstname;
   //last name
-  cout << "Enter your Last name: ";
+  cout << "Enter your Last name: " << endl;
   cin >> lastname;
   //initial balance
-  cout << "Please enter the balance of your saving account: ";
+  cout << "Please enter the balance of your saving account: " << endl;
   cin >> savingsbalance;
   //display name/account type/current balance for said accoutn
-  cout << firstname << " " << lastname << " " << "current balance is: " << savingsbalance;
+  cout << firstname << " " << lastname << " " << "current balance is: " << savingsbalance << endl;
 }
 //checking
 void checking()
@@ -33,12 +33,13 @@ void checking()
   //last name
   //inital balance
   //display name/account type/current balance for said accoutn
-  cout << "Enter your first name: ";
-  cin >> firstname;;
-  cout << "Enter your last name: ";
+  cout << "Enter your first name: " << endl;
+  cin >> firstname;
+  cout << "Enter your last name: " << endl;
   cin >> lastname;
-
-  cout << firstname << " " << lastname << " " << "current balance is: " << balance << "initial balance" << initbalance;
+  cout << "Enter checking balance"<< endl;
+  cin >> balance;
+  cout << firstname << " " << lastname << " " << "current balance is: " << balance << endl;
 }
 
 // deposit
@@ -49,7 +50,7 @@ string user_choice;
   double deposit_amount;
 
     while (true) {
-        cout << "Would you like to make a deposit? [yes/no]: ";
+        cout << "Would you like to make a deposit? [yes/no]: " << endl;
         cin >> user_choice;
 
         if ((user_choice != "Yes") && (user_choice != "yes")) {
@@ -58,13 +59,13 @@ string user_choice;
 
         cout << "1) Checkings" << endl;
         cout << "2) Savings" << endl;
-        cout << "Enter: ";
+        cout << "Enter: " << endl;
         cin >> user_choice2;
 
 
         switch (user_choice2) {
             case 1: {
-                cout << "Enter an amount: ";
+                cout << "Enter an amount: " << endl;
                 cin >> deposit_amount;
                 balance += deposit_amount;
                 cout << "Checkings Balance: " << balance << endl;
@@ -79,12 +80,12 @@ string user_choice;
             }
         }
     }
-    cout << "Would you like to check your balance? [yes/no] ";
+    cout << "Would you like to check your balance? [yes/no] " << endl;
     cin >> user_choice;
 
     cout << "1) Checkings" << endl;
     cout << "2) Savings" << endl;
-    cout << "Enter: ";
+    cout << "Enter: << endl";
     cin >> user_choice2;
 
     if (user_choice2 == 1) {
@@ -108,9 +109,9 @@ void withdraw()
   double widthdraw_amount;
   double current_balance;
   double current_savings;
-  cout << "Would you like to make a withdrawl?";
+  cout << "Would you like to make a withdrawl?" << endl;
   cin >> user_choice;
-  cout << "Which account do you want to proceed with, balance or savings?";
+  cout << "Which account do you want to proceed with, balance or savings?" << endl;
   cin >> user_balance;
 
   if(user_balance=="balance" || "Balance")
@@ -134,10 +135,10 @@ void withdraw()
   {
     while(user_choice=="yes" || user_choice== "Yes")
       {
-        cout << "Enter the amount: ";
+        cout << "Enter the amount: " << endl;
         cin >> widthdraw_amount;
         current_savings= savingsbalance - widthdraw_amount;
-        cout << "Do you wish to make another withdrawl?";
+        cout << "Do you wish to make another withdrawl?" << endl;
         cin >> user_choice;
         if(user_choice == "no" || user_choice == "No")
          {
@@ -157,9 +158,9 @@ void transfer()
   double transfr;
   string ask;
   //Asking which account you want to transfer money from
-  cout<< "Enter 'checking' or 'saving': ";
+  cout<< "Enter 'checking' or 'saving': " << endl;
   cin >> ask;
-  cout << "Type your Transfer amount: ";
+  cout << "Type your Transfer amount: " << endl;
   cin >> transfr;
   if (ask == "checking"){
       balance = balance - transfr;
@@ -193,7 +194,7 @@ int main(){
     string response;
 
   do {
-        cout << "Select one of the following. 1.checkings 2.savings 3.deposit 4.transfer. 5.withdrawal. 6. display. 7.exist"<<endl;
+        cout << "Select one of the following. 1.checkings 2.savings 3.deposit 4.transfer. 5.withdrawal. 6. display. 7.exit. Please enter number or the word"<<endl;
             cin>> response;
 
       if(response == "checkings" || response == "1"){
@@ -219,4 +220,3 @@ int main(){
  cout << "Thank you for banking at Manhattan College Banking and Co!" << endl;
  
 }
-
